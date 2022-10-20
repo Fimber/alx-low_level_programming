fdc10156ecd590443c5f30b973b302ef146aada6
Fimber <fimber6@gmail.com>
2022-10-20 23:23:29 +0100

    fire

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
-   * print_numbers - prints 0 - 9
-    *
-     * Return: void
-    */
+   * print_numbers - a function that prints the numbers, from 0 to 9,
+    * followed by a new line.
+     */
+
 void print_numbers(void)
 {
-		int i;
-
-		for (i = 0; i < 10; i++)
-		{
-			_putchar('0' + i);
-		}
-		_putchar('\n');
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
